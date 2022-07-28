cfefeabcccb39fdca557afe1a6b1169077c55963
Aryan Pandey <margiaryan@gmail.com>
2022-07-28 13:17:34 +0530

    emacs settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,23 +10,27 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(use-package ewal
-  :init (setq ewal-use-built-in-always-p nil
-              ewal-use-built-in-on-failure-p t
-              ewal-built-in-palette "sexy-material"))
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max (* 1024 1024)) ;; 1mb
 
-(use-package ewal-spacemacs-themes
-  :if window-system
-  :ensure t
-  :init
-  (load-theme 'ewal-spacemacs-classic t)
-  (menu-bar-mode -1)
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (window-divider-mode 1))
+(use-package doom-themes
+      :if window-system
+      :ensure t
+      :config
+      (load-theme 'doom-one t)
+      (doom-themes-org-config)
+      (doom-themes-visual-bell-config)
+      (menu-bar-mode -1)
+      (tool-bar-mode -1)
+      (fringe-mode -1)
+      (scroll-bar-mode -1))
 
 (add-to-list 'default-frame-alist
 	     '(font . "JetBrains Mono-11"))
+
+(use-package swiper
+  :ensure t
+  :bind ("C-s" . 'swiper))
 
 (setq use-dialog-box nil)
 (setq use-file-dialog nil)
@@ -40,8 +44,12 @@
   :ensure t
   :config
   (dashboard-setup-startup-hook)
-  (setq dashboard-startup-banner "~/.emacs.d/avatar.png")
-  (setq dashboard-banner-logo-title "I am just a coder for fun"))
+  (setq dashboard-startup-banner 'logo)
+  (setq dashboard-banner-logo-title "I am just a coder for fun")
+  (setq dashboard-footer-messages '("😈 Happy hacking!   "
+                                    "👽 Happy hacking!   "
+                                    "👻 Happy hacking!   "
+                                    "💀 Happy hacking!   ")))
 
 (use-package switch-window
   :ensure t
@@ -72,6 +80,27 @@
   :bind
   ("M-g" . magit-status))
 
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
+
+(use-package ido-vertical-mode
+  :ensure t
+  :init
+  (ido-vertical-mode 1))
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+
+(use-package smex
+  :ensure t
+  :init (smex-initialize)
+  :bind
+  ("M-x" . smex))
+
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode 1))
+
 (defun entry-time-stamp()
   (interactive)
   (insert (format-time-string "<i class='ts'>%a %I:%m.%P %d.%m.%4Y</i>")))
@@ -79,9 +108,13 @@
 ;;evil mode
 (use-package evil
   :ensure t)
+(evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
 (require 'evil)
 (evil-mode 1)
 
+;; Transparency
+(set-frame-parameter (selected-frame) 'alpha '(95))
+(add-to-list 'default-frame-alist '(alpha . (95)))
 ;;which key config
 (use-package which-key
   :ensure t
@@ -124,113 +157,92 @@
 
 (use-package web-mode
   :ensure t
-  :custom-face
-  (css-selector ((t (:inherit default :foreground "#66CCFF"))))
-  (font-lock-comment-face ((t (:foreground "#828282"))))
   :mode
   ("\\.phtml\\'" "\\.tpl\\.php\\'" "\\.[agj]sp\\'" "\\.as[cp]x\\'"
    "\\.erb\\'" "\\.mustache\\'" "\\.djhtml\\'" "\\.[t]?html?\\'"))
 
 ;; company config
 (use-package company
-  :diminish company-mode
-  :hook ((prog-mode LaTeX-mode latex-mode ess-r-mode) . company-mode)
-  :bind
-  (:map company-active-map
-        ([tab] . smarter-tab-to-complete)
-        ("TAB" . smarter-tab-to-complete))
-  :custom
-  (company-minimum-prefix-length 1)
-  (company-tooltip-align-annotations t)
-  (company-require-match 'never)
-  ;; Don't use company in the following modes
-  (company-global-modes '(not shell-mode eaf-mode))
-  ;; Trigger completion immediately.
-  (company-idle-delay 0.1)
-  ;; Number the candidates (use M-1, M-2 etc to select completions).
-  (company-show-numbers t)
+  :ensure t
+  ;; :bind (:map company-active-map ("<tab>" . company-complete-selection))
   :config
-  ;; (unless clangd-p (delete 'company-clang company-backends))
-  (global-company-mode 1)
-  (defun smarter-tab-to-complete ()
-    "Try to `org-cycle', `yas-expand', and `yas-next-field' at current cursor position.
-
-If all failed, try to complete the common part with `company-complete-common'"
-    (interactive)
-    (when yas-minor-mode
-      (let ((old-point (point))
-            (old-tick (buffer-chars-modified-tick))
-            (func-list
-             (if (equal major-mode 'org-mode) '(org-cycle yas-expand yas-next-field)
-               '(yas-expand yas-next-field))))
-        (catch 'func-suceed
-          (dolist (func func-list)
-            (ignore-errors (call-interactively func))
-            (unless (and (eq old-point (point))
-                         (eq old-tick (buffer-chars-modified-tick)))
-              (throw 'func-suceed t)))
-          (company-complete-common))))))
+  (setq company-idle-delay 0.1)
+  (setq company-tooltip-minimum-width 60)
+  (setq company-tooltip-maximum-width 60)
+  (setq company-tooltip-limit 12)
+  (setq company-minimum-prefix-length 3)
+  (setq company-tooltip-align-annotations t))
+(define-key company-active-map (kbd "TAB") 'company-complete-selection)
+(define-key company-active-map (kbd "<tab>") 'company-complete-selection)
+(global-company-mode)
+;; (setq company-backends '((company-ctags company-yasnippet)))
+;; (setq company-idle-delay 0.1)
+;; (setq company-minimum-prefix-length 3)
 
 ;;lsp config
+(setq lsp-keymap-prefix "C-c l")
 (use-package lsp-mode
   :ensure t
-  :defer t
+    :hook ((c-mode          ; clangd
+          c++-mode        ; clangd
+          c-or-c++-mode   ; clangd
+          java-mode       ; eclipse-jdtls
+          js-mode         ; ts-ls (tsserver wrapper)
+          js-jsx-mode     ; ts-ls (tsserver wrapper)
+          typescript-mode ; ts-ls (tsserver wrapper)
+          python-mode     ; pyright
+          web-mode        ; ts-ls/HTML/CSS
+          haskell-mode    ; haskell-language-server
+          ) . lsp-deferred)
+  :hook (lsp-mode . lsp-enable-which-key-integration)
   :commands lsp
-  :custom
-  (lsp-keymap-prefix "C-x l")
-  (lsp-auto-guess-root nil)
-  (lsp-prefer-flymake nil) ; Use flycheck instead of flymake
-  (lsp-enable-file-watchers nil)
-  (lsp-enable-folding nil)
-  (read-process-output-max (* 1024 1024))
-  (lsp-keep-workspace-alive nil)
-  (lsp-eldoc-hook nil)
-  :bind (:map lsp-mode-map ("C-c C-f" . lsp-format-buffer))
-  :hook ((java-mode python-mode go-mode rust-mode
-          js-mode js2-mode typescript-mode web-mode
-          c-mode c++-mode objc-mode) . lsp-deferred)
   :config
-  (defun lsp-update-server ()
-    "Update LSP server."
-    (interactive)
-    ;; Equals to `C-u M-x lsp-install-server'
-    (lsp-install-server t)))
+  (setq lsp-auto-guess-root t)
+  (setq lsp-log-io nil)
+  (setq lsp-restart 'auto-restart)
+  (setq lsp-enable-symbol-highlighting nil)
+  (setq lsp-enable-on-type-formatting nil)
+  (setq lsp-signature-auto-activate nil)
+  (setq lsp-signature-render-documentation nil)
+  (setq lsp-eldoc-hook nil)
+  (setq lsp-modeline-code-actions-enable nil)
+  (setq lsp-modeline-diagnostics-enable nil)
+  (setq lsp-headerline-breadcrumb-enable nil)
+  (setq lsp-semantic-tokens-enable nil)
+  (setq lsp-enable-folding nil)
+  (setq lsp-enable-imenu nil)
+  (setq lsp-enable-snippet t)
+  (setq read-process-output-max (* 1024 1024)) ;; 1MB
+  (setq lsp-idle-delay 0.5))
+;;;;;;;;;;;;;;;;;;; tab width;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq-default tab-width 2)
+(use-package lsp-pyright
+  :ensure t
+  :hook (python-mode . (lambda ()
+                          (require 'lsp-pyright)
+                          (lsp))))  ; or lsp-deferred
+
 
 ;;lsp-ui config
 (use-package lsp-ui
   :ensure t
   :after lsp-mode
-  :diminish
-  :commands lsp-ui-mode
-  :custom-face
-  (lsp-ui-doc-background ((t (:background nil))))
-  (lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic)))))
-  :bind
-  (:map lsp-ui-mode-map
-        ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
-        ([remap xref-find-references] . lsp-ui-peek-find-references)
-        ("C-c u" . lsp-ui-imenu)
-        ("M-i" . lsp-ui-doc-focus-frame))
-  (:map lsp-mode-map
-        ("M-n" . forward-paragraph)
-        ("M-p" . backward-paragraph))
-  :custom
-  (lsp-ui-doc-header t)
-  (lsp-ui-doc-include-signature t)
-  (lsp-ui-doc-border (face-foreground 'default))
-  (lsp-ui-sideline-enable nil)
-  (lsp-ui-sideline-ignore-duplicate t)
-  (lsp-ui-sideline-show-code-actions nil)
   :config
-  ;; Use lsp-ui-doc-webkit only in GUI
-  (when (display-graphic-p)
-    (setq lsp-ui-doc-use-webkit t))
-  ;; WORKAROUND Hide mode-line of the lsp-ui-imenu buffer
-  ;; https://github.com/emacs-lsp/lsp-ui/issues/243
-  (defadvice lsp-ui-imenu (after hide-lsp-ui-imenu-mode-line activate)
-    (setq mode-line-format nil))
-  ;; `C-g'to close doc
-  (advice-add #'keyboard-quit :before #'lsp-ui-doc-hide))
+  (setq lsp-ui-sideline-show-diagnostics t)
+  (setq lsp-ui-sideline-show-code-actions t)
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-doc-use-childframe t)
+  (setq lsp-ui-doc-position 'at-point)
+  (setq lsp-ui-doc-include-signature t)
+  (setq lsp-ui-doc-border (face-foreground 'default))
+  (setq lsp-ui-sideline-show-code-actions nil)
+  (setq lsp-ui-peek-always-show t)
+  (setq lsp-ui-sideline-delay 0.05))
+ 
+
+;;flymake
+(use-package flymake
+  :ensure t)
 
 ;;dap mode
 (use-package dap-mode
@@ -260,26 +272,7 @@ If all failed, try to complete the common part with `company-complete-common'"
 
 ;;yas snippet
 (use-package yasnippet
-  :diminish yas-minor-mode
-  :init
-  (use-package yasnippet-snippets :after yasnippet)
-  :hook ((prog-mode LaTeX-mode org-mode markdown-mode) . yas-minor-mode)
-  :bind
-  (:map yas-minor-mode-map ("C-c C-n" . yas-expand-from-trigger-key))
-  (:map yas-keymap
-        (("TAB" . smarter-yas-expand-next-field)
-         ([(tab)] . smarter-yas-expand-next-field)))
-  :config
-  (yas-reload-all)
-  (defun smarter-yas-expand-next-field ()
-    "Try to `yas-expand' then `yas-next-field' at current cursor position."
-    (interactive)
-    (let ((old-point (point))
-          (old-tick (buffer-chars-modified-tick)))
-      (yas-expand)
-      (when (and (eq old-point (point))
-                 (eq old-tick (buffer-chars-modified-tick)))
-        (ignore-errors (yas-next-field))))))
+  :ensure t)
 
 
 ;;treemacs config
@@ -287,143 +280,19 @@ If all failed, try to complete the common part with `company-complete-common'"
 
 ;; flycheck
 (use-package flycheck
-  :defer t
-  :diminish
-  :hook (after-init . global-flycheck-mode)
-  :commands (flycheck-add-mode)
-  :custom
-  (flycheck-global-modes
-   '(not outline-mode diff-mode shell-mode eshell-mode term-mode))
-  (flycheck-emacs-lisp-load-path 'inherit)
-  (flycheck-indication-mode (if (display-graphic-p) 'right-fringe 'right-margin))
-  :init
-  (if (display-graphic-p)
-      (use-package flycheck-posframe
-        :custom-face
-        (flycheck-posframe-face ((t (:foreground ,(face-foreground 'success)))))
-        (flycheck-posframe-info-face ((t (:foreground ,(face-foreground 'success)))))
-        :hook (flycheck-mode . flycheck-posframe-mode)
-        :custom
-        (flycheck-posframe-position 'window-bottom-left-corner)
-        (flycheck-posframe-border-width 3)
-        (flycheck-posframe-inhibit-functions
-         '((lambda (&rest _) (bound-and-true-p company-backend)))))
-    (use-package flycheck-pos-tip
-      :defines flycheck-pos-tip-timeout
-      :hook (flycheck-mode . flycheck-pos-tip-mode)
-      :custom (flycheck-pos-tip-timeout 30)))
-  :config
-  (use-package flycheck-popup-tip
-    :hook (flycheck-mode . flycheck-popup-tip-mode))
-  (when (fboundp 'define-fringe-bitmap)
-    (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
-      [16 48 112 240 112 48 16] nil nil 'center))
-  (when (executable-find "vale")
-    (use-package flycheck-vale
-      :config
-      (flycheck-vale-setup)
-      (flycheck-add-mode 'vale 'latex-mode))))
-
-;;smartparens
-(use-package smartparens
-  :hook (prog-mode . smartparens-mode)
-  :diminish smartparens-mode
-  :bind
-  (:map smartparens-mode-map
-        ("C-M-f" . sp-forward-sexp)
-        ("C-M-b" . sp-backward-sexp)
-        ("C-M-a" . sp-backward-down-sexp)
-        ("C-M-e" . sp-up-sexp)
-        ("C-M-w" . sp-copy-sexp)
-        ("C-M-k" . sp-change-enclosing)
-        ("M-k" . sp-kill-sexp)
-        ("C-M-<backspace>" . sp-splice-sexp-killing-backward)
-        ("C-S-<backspace>" . sp-splice-sexp-killing-around)
-        ("C-]" . sp-select-next-thing-exchange))
-  :custom
-  (sp-escape-quotes-after-insert nil)
-  :config
-  ;; Stop pairing single quotes in elisp
-  (sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
-  (sp-local-pair 'org-mode "[" nil :actions nil))
-
-;;match parentheis
-;; Show matching parenthesis
-(show-paren-mode 1)
-;; we will call `blink-matching-open` ourselves...
-(remove-hook 'post-self-insert-hook
-             #'blink-paren-post-self-insert-function)
-
-;; this still needs to be set for `blink-matching-open` to work
-(setq blink-matching-paren 'show)
-;; (let ((ov nil)) ; keep track of the overlay
-;;   (advice-add
-;;    #'show-paren-function
-;;    :after
-;;     (defun show-paren--off-screen+ (&rest _args)
-;;       "Display matching line for off-screen paren."
-;;       (when (overlayp ov)
-;;         (delete-overlay ov))
-;;       ;; check if it's appropriate to show match info,
-;;       ;; see `blink-paren-post-self-insert-function'
-;;       (when (and (overlay-buffer show-paren--overlay)
-;;                  (not (or cursor-in-echo-area
-;;                           executing-kbd-macro
-;;                           noninteractive
-;;                           (minibufferp)
-;;                           this-command))
-;;                  (and (not (bobp))
-;;                       (memq (char-syntax (char-before)) '(?\) ?\$)))
-;;                  (= 1 (logand 1 (- (point)
-;;                                    (save-excursion
-;;                                      (forward-char -1)
-;;                                      (skip-syntax-backward "/\\")
-;;                                      (point))))))
-;;         ;; rebind `minibuffer-message' called by
-;;         ;; `blink-matching-open' to handle the overlay display
-;;         (cl-letf (((symbol-function #'minibuffer-message)
-;;                    (lambda (msg &rest args)
-;;                      (let ((msg (apply #'format-message msg args)))
-;;                        (setq ov (display-line-overlay+
-;;                                  (window-start) msg))))))
-;;           (blink-matching-open))))))
+  :ensure t)
+(define-key flymake-mode-map (kbd "M-n") 'flycheck-next-error)
+(define-key flymake-mode-map (kbd "M-p") 'flycheck-previous-error)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-show-quick-access t nil nil "Customized with use-package company")
- '(custom-enabled-themes '(gruvbox-dark-soft))
- '(custom-safe-themes
-   '("83e0376b5df8d6a3fbdfffb9fb0e8cf41a11799d9471293a810deb7586c131e6" default))
- '(hl-todo-keyword-faces
-   '(("TODO" . "#26A8C7")
-     ("NEXT" . "#26A8C7")
-     ("THEM" . "#A7D9E5")
-     ("PROG" . "#60B2C4")
-     ("OKAY" . "#60B2C4")
-     ("DONT" . "#477B8B")
-     ("FAIL" . "#477B8B")
-     ("DONE" . "#37a4c1")
-     ("NOTE" . "#26A8C7")
-     ("KLUDGE" . "#26A8C7")
-     ("HACK" . "#26A8C7")
-     ("TEMP" . "#26A8C7")
-     ("FIXME" . "#26A8C7")
-     ("XXX+" . "#26A8C7")
-     ("\\?\\?\\?+" . "#26A8C7")))
- '(org-fontify-done-headline nil)
- '(org-fontify-todo-headline nil)
  '(package-selected-packages
-   '(gruvbox-theme emmet-mode web-mode lsp-ui company dap-mode yasnippet-snippets lsp-java yasnippet smartparens flycheck-posframe flycheck-vale flycheck-popup-tip highlight-indent-guides which-key evil magit org-bullets hungry-delete switch-window dashboard ewal-spacemacs-themes use-package)))
+   '(mu4e yasnippet-snippets flycheck yasnippet lsp-java lsp-ui lsp-pyright lsp-mode company web-mode emmet-mode highlight-indent-guides which-key evil projectile smex ido-vertical-mode magit org-bullets hungry-delete switch-window dashboard swiper doom-themes use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(css-selector ((t (:inherit default :foreground "#66CCFF"))))
- '(flycheck-posframe-face ((t (:foreground "#37a4c1"))))
- '(flycheck-posframe-info-face ((t (:foreground "#37a4c1"))))
- '(font-lock-comment-face ((t (:foreground "#828282"))))
- '(lsp-ui-doc-background ((t (:background nil))))
- '(lsp-ui-doc-header ((t (:inherit (font-lock-string-face italic))))))
+ )
